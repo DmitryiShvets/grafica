ffa1ba15842856e20be38b99492c07e934335fd4
Dmitrii Shvets <dshvets@sfedu.ru>
2023-09-18 19:56:38 +0300

    Добавил элементы управления для 1 задания

--- Laba2/Task1.Designer.cs
+++ Laba2/Task1.Designer.cs
@@ -29,34 +29,183 @@ namespace Laba2
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.open_file = new System.Windows.Forms.Button();
+            this.source_pix = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gray_pix1 = new System.Windows.Forms.PictureBox();
+            this.gray_pix2 = new System.Windows.Forms.PictureBox();
+            this.compare_pix = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.clear_file = new System.Windows.Forms.Button();
+            this.convert_to_gray = new System.Windows.Forms.Button();
+            this.compare = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.source_pix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gray_pix1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gray_pix2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.compare_pix)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // open_file
             // 
-            this.button1.Location = new System.Drawing.Point(287, 162);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.open_file.Location = new System.Drawing.Point(537, 59);
+            this.open_file.Name = "open_file";
+            this.open_file.Size = new System.Drawing.Size(108, 23);
+            this.open_file.TabIndex = 0;
+            this.open_file.Text = "Выбрать файл";
+            this.open_file.UseVisualStyleBackColor = true;
+            this.open_file.Click += new System.EventHandler(this.open_file_Click);
+            // 
+            // source_pix
+            // 
+            this.source_pix.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.source_pix.Location = new System.Drawing.Point(45, 34);
+            this.source_pix.Name = "source_pix";
+            this.source_pix.Size = new System.Drawing.Size(400, 300);
+            this.source_pix.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.source_pix.TabIndex = 1;
+            this.source_pix.TabStop = false;
+            this.source_pix.Click += new System.EventHandler(this.source_pix_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Исходное изображение";
+            // 
+            // gray_pix1
+            // 
+            this.gray_pix1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gray_pix1.Location = new System.Drawing.Point(733, 34);
+            this.gray_pix1.Name = "gray_pix1";
+            this.gray_pix1.Size = new System.Drawing.Size(400, 300);
+            this.gray_pix1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.gray_pix1.TabIndex = 1;
+            this.gray_pix1.TabStop = false;
+            // 
+            // gray_pix2
+            // 
+            this.gray_pix2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gray_pix2.Location = new System.Drawing.Point(733, 355);
+            this.gray_pix2.Name = "gray_pix2";
+            this.gray_pix2.Size = new System.Drawing.Size(400, 300);
+            this.gray_pix2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.gray_pix2.TabIndex = 1;
+            this.gray_pix2.TabStop = false;
+            // 
+            // compare_pix
+            // 
+            this.compare_pix.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.compare_pix.Location = new System.Drawing.Point(45, 355);
+            this.compare_pix.Name = "compare_pix";
+            this.compare_pix.Size = new System.Drawing.Size(400, 300);
+            this.compare_pix.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.compare_pix.TabIndex = 1;
+            this.compare_pix.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(730, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NTSC RGB";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(730, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "sRGB";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 339);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Разность";
+            // 
+            // clear_file
+            // 
+            this.clear_file.Location = new System.Drawing.Point(537, 97);
+            this.clear_file.Name = "clear_file";
+            this.clear_file.Size = new System.Drawing.Size(108, 23);
+            this.clear_file.TabIndex = 0;
+            this.clear_file.Text = "Очистить";
+            this.clear_file.UseVisualStyleBackColor = true;
+            this.clear_file.Click += new System.EventHandler(this.clear_file_Click);
+            // 
+            // convert_to_gray
+            // 
+            this.convert_to_gray.Location = new System.Drawing.Point(537, 135);
+            this.convert_to_gray.Name = "convert_to_gray";
+            this.convert_to_gray.Size = new System.Drawing.Size(108, 23);
+            this.convert_to_gray.TabIndex = 0;
+            this.convert_to_gray.Text = "Конвертировать";
+            this.convert_to_gray.UseVisualStyleBackColor = true;
+            this.convert_to_gray.Click += new System.EventHandler(this.convert_to_gray_Click);
+            // 
+            // compare
+            // 
+            this.compare.Location = new System.Drawing.Point(537, 174);
+            this.compare.Name = "compare";
+            this.compare.Size = new System.Drawing.Size(108, 23);
+            this.compare.TabIndex = 0;
+            this.compare.Text = "Сравнить";
+            this.compare.UseVisualStyleBackColor = true;
+            this.compare.Click += new System.EventHandler(this.compare_Click);
             // 
             // Task1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gray_pix2);
+            this.Controls.Add(this.gray_pix1);
+            this.Controls.Add(this.compare_pix);
+            this.Controls.Add(this.source_pix);
+            this.Controls.Add(this.compare);
+            this.Controls.Add(this.convert_to_gray);
+            this.Controls.Add(this.clear_file);
+            this.Controls.Add(this.open_file);
             this.Name = "Task1";
             this.Text = "Task1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Task1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.source_pix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gray_pix1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gray_pix2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.compare_pix)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button open_file;
+        private System.Windows.Forms.PictureBox source_pix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox gray_pix1;
+        private System.Windows.Forms.PictureBox gray_pix2;
+        private System.Windows.Forms.PictureBox compare_pix;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button clear_file;
+        private System.Windows.Forms.Button convert_to_gray;
+        private System.Windows.Forms.Button compare;
     }
 }
--- Laba2/Task1.cs
+++ Laba2/Task1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using static System.Net.Mime.MediaTypeNames;
+//using FastBitmap;
 namespace Laba2
 {
     public partial class Task1 : Form
@@ -16,11 +17,10 @@ namespace Laba2
         {
             InitializeComponent();
         }
+        private Bitmap source_image; //Bitmap для открываемого изображения
+        private Bitmap gray_image_1; //Bitmap для открываемого изображения
+        private Bitmap gray_image_2; //Bitmap для открываемого изображения
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void Task1_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -29,6 +29,81 @@ namespace Laba2
                 e.Cancel = true;
                 Hide();
             }
+        }
+
+        private void clear_file_Click(object sender, EventArgs e)
+        {
+            if (source_pix.Image != null)
+            {
+                //source_pix.Image.Dispose();
+                source_pix.Image = null;
+                source_pix.Invalidate();
+            }
+
+        }
+
+        private void open_file_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    source_image = new Bitmap(open_dialog.FileName);
+                    gray_image_1 = new Bitmap(source_image.Width, source_image.Height);
+                    gray_image_2 = new Bitmap(source_image.Width, source_image.Height);
+                    source_pix.Image = source_image;
+                    source_pix.Invalidate();
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void convert_to_gray_Click(object sender, EventArgs e)
+        {
+            using (var fastBitmap = new FastBitmap.FastBitmap(source_image))
+            {
+                for (var x = 0; x < fastBitmap.Width; x++)
+                    for (var y = 0; y < fastBitmap.Height; y++)
+                    {
+                        var gray_1 = new FastBitmap.FastBitmap(gray_image_1);
+                        var gray_2 = new FastBitmap.FastBitmap(gray_image_2);
+
+                        int ntsc = Convert.ToInt32(fastBitmap[x, y].R * 0.3 + fastBitmap[x, y].G * 0.59 + fastBitmap[x, y].B * 0.11);
+                        int srgb = Convert.ToInt32(fastBitmap[x, y].R * 0.21 + fastBitmap[x, y].G * 0.72 + fastBitmap[x, y].B * 0.07);
+                        gray_1[x, y] = Color.FromArgb(
+                            ntsc,
+                            ntsc,
+                            ntsc
+                        );
+                        gray_2[x, y] = Color.FromArgb(
+                            srgb,
+                            srgb,
+                            srgb
+                        );
+                    }
+            }
+            gray_pix1.Image = gray_image_1;
+            gray_pix1.Invalidate();
+            gray_pix2.Image = gray_image_2;
+            gray_pix2.Invalidate();
+
+        }
+
+        private void compare_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void source_pix_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
