5d23a2c3c75e91a883683ef7c53cf6fa49b275a4
Данил Березенцев <danil.ber@mail.ru>
2023-09-18 19:55:28 +0300

    Сделал выделение канала по цветам(2 задание)

--- Laba2/Task2.Designer.cs
+++ Laba2/Task2.Designer.cs
@@ -29,34 +29,100 @@ namespace Laba2
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.button_LoadImage = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button_red = new System.Windows.Forms.Button();
+            this.button_green = new System.Windows.Forms.Button();
+            this.button_blue = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // button_LoadImage
             // 
-            this.button1.Location = new System.Drawing.Point(312, 143);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button2";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button_LoadImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_LoadImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button_LoadImage.Location = new System.Drawing.Point(824, 12);
+            this.button_LoadImage.Name = "button_LoadImage";
+            this.button_LoadImage.Size = new System.Drawing.Size(224, 87);
+            this.button_LoadImage.TabIndex = 0;
+            this.button_LoadImage.Text = "Загрузить изображение";
+            this.button_LoadImage.UseVisualStyleBackColor = true;
+            this.button_LoadImage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(806, 581);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // button_red
+            // 
+            this.button_red.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_red.Enabled = false;
+            this.button_red.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button_red.Location = new System.Drawing.Point(824, 105);
+            this.button_red.Name = "button_red";
+            this.button_red.Size = new System.Drawing.Size(224, 62);
+            this.button_red.TabIndex = 2;
+            this.button_red.Text = "Выделить красный канал";
+            this.button_red.UseVisualStyleBackColor = true;
+            this.button_red.Click += new System.EventHandler(this.button_red_Click);
+            // 
+            // button_green
+            // 
+            this.button_green.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_green.Enabled = false;
+            this.button_green.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button_green.Location = new System.Drawing.Point(824, 173);
+            this.button_green.Name = "button_green";
+            this.button_green.Size = new System.Drawing.Size(224, 62);
+            this.button_green.TabIndex = 3;
+            this.button_green.Text = "Выделить зеленый канал";
+            this.button_green.UseVisualStyleBackColor = true;
+            this.button_green.Click += new System.EventHandler(this.button_green_Click);
+            // 
+            // button_blue
+            // 
+            this.button_blue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_blue.Enabled = false;
+            this.button_blue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button_blue.Location = new System.Drawing.Point(824, 241);
+            this.button_blue.Name = "button_blue";
+            this.button_blue.Size = new System.Drawing.Size(224, 62);
+            this.button_blue.TabIndex = 4;
+            this.button_blue.Text = "Выделить синий канал";
+            this.button_blue.UseVisualStyleBackColor = true;
+            this.button_blue.Click += new System.EventHandler(this.button_blue_Click);
             // 
             // Task2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1060, 605);
+            this.Controls.Add(this.button_blue);
+            this.Controls.Add(this.button_green);
+            this.Controls.Add(this.button_red);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.button_LoadImage);
+            this.MinimumSize = new System.Drawing.Size(1076, 644);
             this.Name = "Task2";
             this.Text = "Task2";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Task2_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_LoadImage;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button_red;
+        private System.Windows.Forms.Button button_green;
+        private System.Windows.Forms.Button button_blue;
     }
 }
--- Laba2/Task2.cs
+++ Laba2/Task2.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FastBitmap;
 
 namespace Laba2
 {
     public partial class Task2 : Form
     {
+        private Image image;
+        private Bitmap editedImage;
+
         public Task2()
         {
             InitializeComponent();
@@ -19,7 +23,21 @@ namespace Laba2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg, *.png)|*.jpg;*.png";
 
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Загрузка изображения
+                image = Image.FromFile(openFileDialog.FileName);
+                pictureBox.Image = image;
+
+                // Создание Bitmap для редактирования и отображения
+                editedImage = new Bitmap(image.Width, image.Height);
+                button_red.Enabled = true;
+                button_green.Enabled = true;
+                button_blue.Enabled = true;
+            }
         }
 
         private void Task2_FormClosing(object sender, FormClosingEventArgs e)
@@ -29,6 +47,53 @@ namespace Laba2
                 e.Cancel = true;
                 Hide();
             }
+        }
+
+        private void button_red_Click(object sender, EventArgs e)
+        {
+            ExtractChannel("red");
+        }
+
+        private void button_green_Click(object sender, EventArgs e)
+        {
+            ExtractChannel("green");
+        }
+
+        private void button_blue_Click(object sender, EventArgs e)
+        {
+            ExtractChannel("blue");
+        }
+
+        private void ExtractChannel(string color)
+        {
+            // Проход по каждому пикселю изображения
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    // Получение цвета пикселя
+                    Color pixelColor = ((Bitmap)image).GetPixel(x, y);
+                    Color newColor = Color.White;
+                    switch (color)
+                    {
+                        case "red":
+                            newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                            break;
+                        case "green":
+                            newColor = Color.FromArgb(0, pixelColor.G, 0);
+                            break;
+                        case "blue":
+                            newColor = Color.FromArgb(0, 0, pixelColor.B);
+                            break;
+                    }
+
+                    // Установка нового цвета для пикселя в отредактированном изображении
+                    editedImage.SetPixel(x, y, newColor);
+                }
+            }
+
+            // Отображение отредактированного изображения на PictureBox
+            pictureBox.Image = editedImage;
         }
     }
 }
