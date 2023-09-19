26f05162c1af4e8627163a4e34de9de32170ee67
Dmitrii Shvets <dshvets@sfedu.ru>
2023-09-19 19:05:42 +0300

    Добавил сравнение двую оттенков серого.

--- Laba2/Task1.cs
+++ Laba2/Task1.cs
@@ -17,9 +17,10 @@ namespace Laba2
         {
             InitializeComponent();
         }
-        private Bitmap source_image; //Bitmap для открываемого изображения
-        private Bitmap gray_image_1; //Bitmap для открываемого изображения
-        private Bitmap gray_image_2; //Bitmap для открываемого изображения
+        private Bitmap source_image;
+        private Bitmap gray_image_1;
+        private Bitmap gray_image_2;
+        private Bitmap compare_image;
 
 
         private void Task1_FormClosing(object sender, FormClosingEventArgs e)
@@ -33,16 +34,23 @@ namespace Laba2
 
         private void clear_file_Click(object sender, EventArgs e)
         {
-            if (source_pix != null)
-            {
-                source_pix.Image = null;
-                source_pix.Invalidate();
-                gray_pix1.Image = null;
-                gray_pix1.Invalidate();
-                gray_pix2.Image = null;
-                gray_pix2.Invalidate();
-            }
+
+            source_pix.Image = null;
+            source_pix.Invalidate();
+
+            gray_pix1.Image = null;
+            gray_pix1.Invalidate();
+
+            gray_pix2.Image = null;
+            gray_pix2.Invalidate();
+
+            compare_pix.Image = null;
+            compare_pix.Invalidate();
+
             source_image = null;
+            gray_image_1 = null;
+            gray_image_2 = null;
+            compare_image = null;
         }
 
         private void open_file_Click(object sender, EventArgs e)
@@ -57,6 +65,8 @@ namespace Laba2
                     source_image = new Bitmap(open_dialog.FileName);
                     gray_image_1 = new Bitmap(source_image.Width, source_image.Height);
                     gray_image_2 = new Bitmap(source_image.Width, source_image.Height);
+                    compare_image = new Bitmap(source_image.Width, source_image.Height);
+
                     source_pix.Image = source_image;
                     source_pix.Invalidate();
                 }
@@ -82,18 +92,16 @@ namespace Laba2
                                 for (var y = 0; y < fastBitmap.Height; y++)
                                 {
 
-                                    int ntsc = Convert.ToInt32(fastBitmap[x, y].R * 0.3 + fastBitmap[x, y].G * 0.59 + fastBitmap[x, y].B * 0.11);
-                                    int srgb = Convert.ToInt32(fastBitmap[x, y].R * 0.21 + fastBitmap[x, y].G * 0.72 + fastBitmap[x, y].B * 0.07);
-                                    fast_gray_1[x, y] = Color.FromArgb(
-                                        ntsc,
-                                        ntsc,
-                                        ntsc
-                                    );
-                                    fast_gray_2[x, y] = Color.FromArgb(
-                                        srgb,
-                                        srgb,
-                                        srgb
-                                    );
+                                    int ntsc = Convert.ToInt32(fastBitmap[x, y].R * 0.3 +
+                                                               fastBitmap[x, y].G * 0.59 +
+                                                               fastBitmap[x, y].B * 0.11);
+
+                                    int srgb = Convert.ToInt32(fastBitmap[x, y].R * 0.21 +
+                                                               fastBitmap[x, y].G * 0.72 +
+                                                               fastBitmap[x, y].B * 0.07);
+
+                                    fast_gray_1[x, y] = Color.FromArgb(ntsc, ntsc, ntsc);
+                                    fast_gray_2[x, y] = Color.FromArgb(srgb, srgb, srgb);
                                 }
                         }
                     }
@@ -102,15 +110,51 @@ namespace Laba2
                 gray_pix1.Invalidate();
                 gray_pix2.Image = gray_image_2;
                 gray_pix2.Invalidate();
+                compare_pix.Image = null;
+                compare_pix.Invalidate();
+
             }
         }
 
         private void compare_Click(object sender, EventArgs e)
         {
-            source_pix.Image = source_image;
-            source_pix.Invalidate();
+            if (gray_image_1 != null && gray_image_2 != null)
+            {
+                int min = 255;
+                int max = 0;
+
+                using (var fast_comp = new FastBitmap(compare_image))
+                {
+                    using (var fast_gray_1 = new FastBitmap(gray_image_1))
+                    {
+                        using (var fast_gray_2 = new FastBitmap(gray_image_2))
+                        {
+                            for (var x = 0; x < fast_comp.Width; x++)
+                                for (var y = 0; y < fast_comp.Height; y++)
+                                {
+                                    var color = Math.Abs(fast_gray_1[x, y].R - fast_gray_2[x, y].R);
+                                    fast_comp[x, y] = Color.FromArgb(color, color, color);
+                                    if (color > max) max = color;
+                                    if (color < min) min = color;
+                                }
+                        }
+                    }
+                    for (var x = 0; x < fast_comp.Width; x++)
+                        for (var y = 0; y < fast_comp.Height; y++)
+                        {
+                            var lin = LinX(Convert.ToInt32(fast_comp[x, y].R), min, max);
+                            fast_comp[x, y] = Color.FromArgb(lin, lin, lin);
+                        }
+                }
+                compare_pix.Image = compare_image;
+                compare_pix.Invalidate();
+            }
         }
 
+        private int LinX(int x, int a, int b)
+        {
+            return (x - a) * 255/(b - a);
+        }
         private void source_pix_Click(object sender, EventArgs e)
         {
 
