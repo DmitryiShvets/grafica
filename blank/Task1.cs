a0f860b7bf6123eeec8f6b25b57435c3f2dc77e1
Dmitrii Shvets <dshvets@sfedu.ru>
2023-09-22 21:46:32 +0300

    Изменена заготовка для будущих проектов.

---
+++ blank/Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FastBitmaps;
+namespace blank
+{
+    public partial class Task1 : Form
+    {
+        public Task1()
+        {
+            InitializeComponent();
+        }
+
+        private void Task1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+
+            }
+        }
+    }
+}
