1fdfca8105d659fc98266bdd009a8c6c4f2471ee
Dmitrii Shvets <dshvets@sfedu.ru>
2023-11-15 16:05:17 +0300

    Добавлено: загатовка для лабы 11

---
+++ Laba11/src/renderer.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "buffer_objects.h"
+#include "shader_program.h"
+
+class Renderer {
+public:
+    static void draw(const VAO& vao, const EBO& ebo);
+
+    static void draw(const VAO& vao);
+
+    static void draw(VAO* vao);
+
+    static void setClearColor(float r, float g, float b, float a);
+
+    static void clear();
+
+    static void setViewPort(int x, int y, int width, int height);
+};
---
+++ Laba11/src/resource_manager.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+#include <map>
+#include "shader_program.h"
+#include "buffer_objects.h"
+
+class ResourceManager {
+public:
+    static ResourceManager& getInstance();
+
+    ~ResourceManager();
+
+    void init();
+
+    void destroy();
+
+    ShaderProgram& getProgram(const std::string& progName);
+
+    VAO baseVAO;
+
+private:
+    ResourceManager();
+
+    ResourceManager(const ResourceManager&) = delete;
+
+    ResourceManager& operator=(const ResourceManager&) = delete;
+
+    ResourceManager& operator=(ResourceManager&& program) = delete;
+
+    ResourceManager(ResourceManager&& program) = delete;
+
+    std::map<std::string, ShaderProgram> shaderPrograms;
+
+};
